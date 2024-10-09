95b7bde142699ef8803d24d889fd5f69dc760ba9
moahamedlamineremini <mohamed.remini@etudiant.univ-lr.fr>
2024-10-09 09:57:49 +0200

    probleme synchro

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 
 const server = index.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  console.log('tota');
 });
 
 export default server;
