fed38b8b0ef2b139c37eca69694f31c2fb13f5e0
amiramiour <amiouramirtahar@gmail.com>
2024-10-09 11:30:54 +0200

    interface améliorée

--- ui/src/components/SubmitReactionTime.js
+++ ui/src/components/SubmitReactionTime.js
@@ -1,29 +1,60 @@
 // src/components/SubmitReactionTime.js
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
 const SubmitReactionTime = ({ token }) => {
-  const [time, setTime] = useState('');
+  const [message, setMessage] = useState('Click "Start" to begin');
+  const [showStopButton, setShowStopButton] = useState(false);
+  const [reactionTime, setReactionTime] = useState(0);
+  const startTimeRef = useRef(null);
+  const timerRef = useRef(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post('http://localhost:3000/api/reaction-time/submit-reaction-time', { time }, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      console.log('Reaction time submitted successfully');
-    } catch (error) {
-      console.error('Error submitting reaction time', error);
+  const handleStart = () => {
+    setMessage('Wait for "Stop" button to appear...');
+    setShowStopButton(false);
+    setReactionTime(0);
+    const delay = Math.floor(Math.random() * 3000) + 2000; // Delay between 2s and 5s
+
+    timerRef.current = setTimeout(() => {
+      startTimeRef.current = Date.now();
+      setMessage('Click "Stop" now!');
+      setShowStopButton(true);
+    }, delay);
+  };
+
+  const handleStop = async () => {
+    if (startTimeRef.current) {
+      const reactionTime = Date.now() - startTimeRef.current;
+      setReactionTime(reactionTime);
+      setMessage(`Your reaction time is ${reactionTime} ms`);
+
+      try {
+        await axios.post('http://localhost:3000/api/reaction-time/submit-reaction-time', { time: reactionTime }, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log('Reaction time submitted successfully');
+      } catch (error) {
+        console.error('Error submitting reaction time', error);
+      }
+
+      startTimeRef.current = null;
+      clearTimeout(timerRef.current);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="number" value={time} onChange={(e) => setTime(e.target.value)} placeholder="Reaction Time (ms)" required />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <p>{message}</p>
+      <div className="button-container">
+        <button className="start" onClick={handleStart}>Start</button>
+        {showStopButton && <button className="stop" onClick={handleStop}>Stop</button>}
+      </div>
+      <div className="reaction-tube">
+        <div className="reaction-tube-fill" style={{ width: `${reactionTime / 10}%` }}></div>
+      </div>
+    </div>
   );
 };
 
